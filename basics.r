ecf7535731e67816906666e4478249b68a30cfa7
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-04-30 00:08:16 +0530

    work with sequences

--- basics.r
+++ basics.r
@@ -28,3 +28,22 @@ class("this is a string")
 class(5 == 3) # logicals
 
 print("---------") # endling line
+
+
+# FOUR: Work with sequences
+seq = c(1, 2, 3, 4) # creates a sequence (array) containing these values
+seq 
+class(seq)
+seq = c(1, "a", FALSE)
+seq
+class(seq) # character, as non-characters gets casted into characters
+
+my_landline = c(0,3,3,2,2,8,4,4,2,9,8)
+table(my_landline) # prints the freqeuency of each entry in a sequence
+seq = c('e','x','a','g','a','r','r','a','t','i','o','n')
+table(seq)
+class(seq)
+
+print("---------") # endling line
+
+
