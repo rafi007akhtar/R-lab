518ac57f564540a3ea15892a0ac0aa31264f82a8
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-04-29 21:03:35 +0530

    exploring murders dataset of dslabs package

---
+++ murders.r
@@ -0,0 +1,15 @@
+# Explore the murders dataset in dslabs package
+
+# Load the "murders" dataset from dslabs package
+library(dslabs)
+data(murders)
+
+# murders # display the entire dataset in table-form
+
+paste("The murders dataset is of the class", class(murders), "with the following structure.")
+
+# Display its strucutre
+str(murders)
+
+# Show the first 6 entries of this dataset
+head(murders)
