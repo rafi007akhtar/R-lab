c1f7ccc046bbd18a4ea45105c4b48e1329114dec
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-04-30 18:02:40 +0530

    sort and order

--- murders.r
+++ murders.r
@@ -14,5 +14,21 @@ str(murders)
 # Show the first 6 entries of this dataset
 head(murders)
 
+# Print the regions class
 regions = murders$region 
-class(regions) # factor -- used to store categorical data
+class(regions) # FACTOR -- used to store categorical data
+
+# Sort the total murders in ascending order
+totals = murders$total
+print("Total murders")
+totals
+print("Sorted murders")
+sort(totals)
+
+# Display the state names according to their total murders in ascending order
+orders = order(totals) # returns the multivalued index vector required to sort the integers
+states = murders$state[orders] # access the state column in murders, and index them as per their total murders order
+print("States from lowest murder rate till highest")
+print(states)
+
+
